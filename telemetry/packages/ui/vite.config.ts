c4a0beffb76f5eadfea4323fbb2c1ad9dc0a6d9c
David Beechey <david@dtbeechey.dev>
2023-09-22 14:33:50 +0100

    fix(tel-ui): openmct-lib error
    Fixed bug with openmct-lib static copy (the switch to `pnpm` changed the `node_modules` dir)

--- telemetry/packages/ui/vite.config.ts
+++ telemetry/packages/ui/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig(({ command, mode }) => {
       viteStaticCopy({
         targets: [
           {
-            src: '../../node_modules/openmct/dist/*',
+            src: './node_modules/openmct/dist/*',
             dest: 'openmct-lib',
           },
         ],
