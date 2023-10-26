f22f5701968b486cfc508edcbb071eeebfc279de
Leo Foulds <leofoulds@virginmedia.com>
2023-10-26 19:18:56 +0100

    PID

--- lib/motors/PID.cpp
+++ lib/motors/PID.cpp
@@ -6,7 +6,6 @@ void PIDController_Init(PIDController *pid) {
     /* Clear controller variables */
 	pid->integrator = 0.0f;
 	pid->prevError  = 0.0f;
-
 	pid->differentiator  = 0.0f;
 	pid->prevMeasurement = 0.0f;
 
