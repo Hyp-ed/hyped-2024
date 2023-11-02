a8240253081d7ab818e8e7538562ebace6deb46f
licornes-fluos <54073764+licornes-fluos@users.noreply.github.com>
2023-11-02 19:28:32 +0000

    initial commit

---
+++ lib/sensors/optical_flow.cpp
@@ -0,0 +1,5 @@
+#include "optical_flow.hpp"
+
+namespace hyped::io {
+    
+}
---
+++ lib/sensors/optical_flow.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cstdint>
+#include <cstring>
+#include <memory>
+#include <optional>
+
+#include <core/logger.hpp>
+#include <core/types.hpp>
+#include <io/hardware_spi.hpp>
+
+namespace hyped::io {
+    
+}
