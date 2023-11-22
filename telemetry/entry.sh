1fab196681b403719e5ef6a4a92536bf9aacaa6b
David Beechey <david@dtbeechey.dev>
2023-11-22 22:01:38 +0000

    fix env files
    I keep doing silly things...

--- telemetry/entry.sh
+++ telemetry/entry.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Use docker env files
-cp /usr/src/app/server/.env.docker /usr/src/app/server/.env
-cp /usr/src/app/ui/.env.docker /usr/src/app/ui/.env
+cp /usr/src/app/packages/server/.env.docker /usr/src/app/packages/server/.env
+cp /usr/src/app/packages/ui/.env.docker /usr/src/app/packages/ui/.env
 
 yarn build
 
