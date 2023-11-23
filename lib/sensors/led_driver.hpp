fd8910ca2c591c6205d679e0acb63e873a352927
H-Allen <harveyjallen1@gmail.com>
2023-11-23 18:16:52 +0000

    creating led_driver files

---
+++ lib/sensors/led_driver.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "i2c_sensors.hpp"
+
+#include <unistd.h>
+
+#include <cstdint>
+#include <memory>
+#include <optional>
+
+#include <core/logger.hpp>
+#include <core/types.hpp>
+#include <core/wall_clock.hpp>
+#include <io/i2c.hpp>
+
+namespace hyped::sensors {
+    
+}
