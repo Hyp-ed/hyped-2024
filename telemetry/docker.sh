cbb515e078ea2512f8fb677b0ad244d2828e2202
David Beechey <david@dtbeechey.dev>
2023-11-25 13:45:35 +0000

    feat(tel-docker): Add flag for running with MQTT broker

--- telemetry/docker.sh
+++ telemetry/docker.sh
@@ -1,18 +1,70 @@
 #!/bin/bash
 
+# Function to display script usage
+usage() {
+ echo "Usage: $0 <yarn_script> [OPTIONS]"
+ echo "Options:"
+ echo " -h, --help              Display this help message"
+ echo " -b, --build             Build the docker image before running the container"
+ echo " -m, --with-mqtt-broker  Run the MQTT broker along with the container"
+}
+
+has_argument() {
+    [[ ("$1" == *=* && -n ${1#*=}) || ( ! -z "$2" && "$2" != -*)  ]];
+}
+
+extract_argument() {
+  echo "${2:-${1#*=}}"
+}
+
+# Default values for options
+build=false
+with_mqtt_broker=false
+
 if [ -z "$1" ]; then
-  echo "Usage: $0 <yarn_script>"
+  echo "Missing argument: yarn_script" >&2
+  usage
   exit 1
 fi
 
 export YARN_SCRIPT=$1
+shift
 
-echo "Running with yarn script: $YARN_SCRIPT"
+# Function to handle options and arguments
+handle_options() {
+  while [ $# -gt 0 ]; do
+    case $1 in
+      -h | --help)
+        usage
+        exit 0
+        ;;
+      -b | --build)
+        build=true
+        ;;
+      -m | --with-mqtt-broker)
+        with_mqtt_broker=true
+        ;;
+      *)
+        echo "Invalid option: $1" >&2
+        usage
+        exit 1
+        ;;
+    esac
+    shift
+  done
+}
 
-if [ "$2" == "--build" ]; then
-  echo "Building docker image"
-  docker-compose up --build
+# Main script execution
+handle_options "$@"
+
+if [ "$build" = true ]; then
+  if [ "$with_mqtt_broker" = true ]; then
+    docker-compose -f docker-compose.yml -f docker-compose.mqtt.yml up --build
+  else
+    docker-compose up --build
+  fi
+elif [ "$with_mqtt_broker" = true ]; then
+  docker-compose -f docker-compose.yml -f docker-compose.mqtt.yml up
 else
-  echo "Using cached docker image"
   docker-compose up
 fi
