2c9a4f15833330358ec62578b3424005025cf89c
Dylan Cavers <s2179041@ed.ac.uk>
2023-11-25 21:03:17 +0000

    cleanup merge

--- telemetry/packages/server/src/app.module.ts
+++ telemetry/packages/server/src/app.module.ts
@@ -12,10 +12,7 @@ import { PodControlsModule } from './modules/controls/PodControls.module';
 import { WarningsModule } from './modules/warnings/Warnings.module';
 import { RemoteLogsModule } from './modules/remote-logs/RemoteLogs.module';
 import { PublicDataModule } from './modules/public-data/PublicData.module';
-<<<<<<< HEAD
-=======
 import { LiveLogsGateway } from './modules/live-logs/LiveLogs.gateway';
->>>>>>> infra-mqtt_tests
 
 @Module({
   imports: [
