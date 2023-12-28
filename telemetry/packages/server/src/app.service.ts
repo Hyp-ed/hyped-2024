eb115fba85c0a10f86daf9cb35faf22305097041
David Beechey <david@dtbeechey.dev>
2023-12-28 21:43:58 +0000

    feat(tel-server): current git branch endpoint

--- telemetry/packages/server/src/app.service.ts
+++ telemetry/packages/server/src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { exec } from 'child_process';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  /**
+   * Returns the current branch of the server using child_process.
+   */
+  public getBranch() {
+    return new Promise<string>((resolve, reject) => {
+      exec('git rev-parse --abbrev-ref HEAD', (err, stdout) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(stdout.trim());
+        }
+      });
+    });
   }
 }
