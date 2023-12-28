2d3f936ca9f3c9133e67b6c55094e9fb4c0b1d1d
David Beechey <david@dtbeechey.dev>
2023-12-28 23:20:38 +0000

    feat(tel-ui): make sidebar resizable

--- telemetry/packages/ui/app/App.tsx
+++ telemetry/packages/ui/app/App.tsx
@@ -2,27 +2,40 @@ import { useState } from 'react';
 import { VIEWS } from './views';
 import { ControlsUI } from './components/controls';
 import { cn } from './lib/utils';
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@/components/ui/resizable';
 
 const App = () => {
   const [selectedComponent, setSelectedComponent] = useState(0);
 
   return (
-    <div className="grid grid-cols-8 bg-hyped-background">
-      {VIEWS.map((component) => (
-        <div
-          className={cn(
-            'h-[100vh] w-full col-span-7 p-1',
-            !(VIEWS.indexOf(component) === selectedComponent) && 'hidden',
-          )}
-        >
-          {component.component}
-        </div>
-      ))}
-      <ControlsUI
-        selectedComponent={selectedComponent}
-        setSelectedComponent={setSelectedComponent}
-      />
-    </div>
+    <ResizablePanelGroup
+      direction="horizontal"
+      className="w-full bg-hyped-background"
+    >
+      <ResizablePanel defaultSize={85} minSize={50}>
+        {VIEWS.map((component) => (
+          <div
+            className={cn(
+              'h-[100vh] w-full col-span-7 p-1',
+              !(VIEWS.indexOf(component) === selectedComponent) && 'hidden',
+            )}
+          >
+            {component.component}
+          </div>
+        ))}
+      </ResizablePanel>
+      <ResizableHandle withHandle className="bg-openmct-light-gray" />
+      <ResizablePanel defaultSize={15} minSize={10}>
+        <ControlsUI
+          selectedComponent={selectedComponent}
+          setSelectedComponent={setSelectedComponent}
+        />
+      </ResizablePanel>
+    </ResizablePanelGroup>
   );
 };
 
