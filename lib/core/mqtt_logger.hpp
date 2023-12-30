cab28799d0289f0e191bd911476df7f56e8fefe2
DylanCavers <s2179041@ed.ac.uk>
2023-12-30 14:52:28 +0000

    implement mqtt logging

--- lib/core/mqtt_logger.hpp
+++ lib/core/mqtt_logger.hpp
@@ -10,6 +10,7 @@ class MqttLogger : public ILogger {
   MqttLogger(const char *const label,
              const LogLevel level,
              const core::ITimeSource &timer,
+             Logger logger,
              std::shared_ptr<IMqtt> mqtt);
   void log(const LogLevel level, const char *format, ...);
 
@@ -17,6 +18,7 @@ class MqttLogger : public ILogger {
   const char *const label_;
   const LogLevel level_;
   const core::ITimeSource &time_source_;
+  std::shared_ptr<Logger> file_logger_;
   std::shared_ptr<IMqtt> mqtt_;
 };
 }  // namespace hyped::core
