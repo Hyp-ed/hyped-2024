bcf2a75119bb93beb0a0f002d81631a1218616ad
pkhudov <pkhudov@gmail.com>
2024-01-06 17:16:39 +0000

    Squashed commit of erase_kalman_template

--- lib/navigation/filtering/extended_kalman_filter.hpp
+++ lib/navigation/filtering/extended_kalman_filter.hpp
@@ -7,27 +7,16 @@
 #include <Eigen/Dense>
 #include <core/time.hpp>
 #include <core/types.hpp>
+#include <navigation/control/consts.hpp>
 
 namespace hyped::navigation {
 
 // State dimension 3, measurement dimension 1, extended dimension 2
-template<std::size_t state_dimension,
-         std::size_t measurement_dimension,
-         std::size_t extended_dimension>
 class ExtendedKalmanFilter {
  public:
   ExtendedKalmanFilter(std::shared_ptr<core::ITimeSource> time_source,
                        const StateVector initial_state,
-                       const ErrorCovarianceMatrix initial_error_covariance)
-      : time_source_(time_source),
-        last_update_time_(time_source->now()),
-        state_estimate_(initial_state),
-        error_covariance_(initial_error_covariance)
-  {
-    static_assert(state_dimension > 0);
-    static_assert(measurement_dimension > 0);
-    static_assert(extended_dimension > 0);
-  }
+                       const ErrorCovarianceMatrix initial_error_covariance);
 
   void filter(const StateTransitionMatrix &transition_matrix,
               const StateTransitionCovarianceMatrix &transition_covariance,
@@ -35,22 +24,7 @@ class ExtendedKalmanFilter {
               const MeasurementNoiseCovarianceMatrix &measurement_noise_covariance,
               const MeasurementVector &measurement,
               const JacobianMatrix &jacobian_matrix,
-              const ExtendedStateVector &extended_state_vector)
-  {
-    // TODOLater: add noise to propogation step
-    const auto prop_state_estimate
-      = transition_matrix * state_estimate_ + jacobian_matrix * extended_state_vector;
-    const auto prop_error_covariance
-      = (transition_matrix.transpose() * error_covariance_ * transition_matrix)
-        + transition_covariance;
-    const auto kalman_gain
-      = prop_error_covariance * measurement_matrix.transpose()
-        * (measurement_matrix * prop_error_covariance * measurement_matrix.transpose()
-           + measurement_noise_covariance)
-            .inverse();
-    state_estimate_ = prop_state_estimate
-                      + kalman_gain * (measurement - measurement_matrix * prop_state_estimate);
-  }
+              const ExtendedStateVector &extended_state_vector);
 
   const StateVector &getStateEstimate() const { return state_estimate_; }
   const ErrorCovarianceMatrix &getErrorCovariance() const { return error_covariance_; }
---
+++ lib/navigation/filtering/kalman_filter.cpp
@@ -0,0 +1,38 @@
+#include "kalman_filter.hpp"
+
+namespace hyped::navigation {
+
+KalmanFilter::KalmanFilter(std::shared_ptr<core::ITimeSource> time_source,
+                           const StateVector initial_state,
+                           const ErrorCovarianceMatrix initial_error_covariance)
+    : time_source_(time_source),
+      last_update_time_(time_source->now()),
+      state_estimate_(initial_state),
+      error_covariance_(initial_error_covariance)
+{
+  static_assert(state_dimension > 0);
+  static_assert(measurement_dimension > 0);
+}
+
+void KalmanFilter::filter(const StateTransitionMatrix &transition_matrix,
+                          const StateTransitionCovarianceMatrix &transition_covariance,
+                          const MeasurementMatrix &measurement_matrix,
+                          const MeasurementNoiseCovarianceMatrix &measurement_noise_covariance,
+                          const MeasurementVector &measurement)
+{
+  // TODOLater: figure out how to make transition matrix given time delta - in main nav section
+  const auto prop_state_estimate = transition_matrix * state_estimate_;
+  const auto prop_error_covariance
+    = (transition_matrix.transpose() * error_covariance_ * transition_matrix)
+      + transition_covariance;
+  const auto innovation_covariance = prop_error_covariance * measurement_matrix.transpose();
+  // TODOLater: Some optimisation is to be found here:
+  // 1. Try and get rid of the inverse.
+  const auto kalman_gain
+    = innovation_covariance
+      * (measurement_matrix * innovation_covariance + measurement_noise_covariance).inverse();
+  state_estimate_
+    = prop_state_estimate + kalman_gain * (measurement - measurement_matrix * prop_state_estimate);
+}
+
+}  // namespace hyped::navigation
