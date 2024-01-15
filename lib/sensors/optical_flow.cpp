e29779b11fe7b17d596d296a5acbe82cbe7d7a42
licornes-fluos <54073764+licornes-fluos@users.noreply.github.com>
2024-01-15 17:33:55 +0000

    read registers

--- lib/sensors/optical_flow.cpp
+++ lib/sensors/optical_flow.cpp
@@ -1,5 +1,10 @@
 #include "optical_flow.hpp"
 
 namespace hyped::io {
-    
+std::uint8_t OpticalFLow::getPosition()
+{
+	// call function read(kXLowReg, kXLow, 8)
+	// nb of bytes is an assumption, not mentioned in datasheet
+	// do same for Y
+}
 }
--- lib/sensors/optical_flow.hpp
+++ lib/sensors/optical_flow.hpp
@@ -7,8 +7,22 @@
 
 #include <core/logger.hpp>
 #include <core/types.hpp>
-#include <io/hardware_spi.hpp>
+#include <io/spi.hpp>
 
 namespace hyped::io {
-    
+
+class OpticalFLow {
+// start with initialising the sensor (method 1)
+// gets the value of x_low and of y_low and return it, so read from 0x03 and 0x05 (method 2)
+// this will use read, addr: is register (either x or y), pointer: pointer to head of read buffer ??
+// and len: number of bytes to be read, 16 bits?
+
+private:
+std::uint8_t getPosition();
+//Register addresses for x and y
+static constexpr std::uint8_t kXLowReg = 0x03;
+static constexpr std::uint8_t kYLowReg = 0x05;
+static constexpr std::uint8_t kXLow = 0;
+static constexpr std::uint8_t kYLow = 0;
+};
 }
