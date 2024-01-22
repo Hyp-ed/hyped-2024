ed8498ba9dedf33ff7077a76b608e0a450fbec74
H-Allen <harveyjallen1@gmail.com>
2024-01-22 16:07:19 +0000

    formatting

--- lib/sensors/led_driver.cpp
+++ lib/sensors/led_driver.cpp
@@ -6,24 +6,25 @@ std::optional<LedDriver> LedDriver::create(core::ILogger &logger,
                                            std::shared_ptr<io::II2c> i2c,
                                            const std::uint8_t device_address)
 {
-    if (device_address != kDefaultLedDriverAddress) {
-        logger.log(core::LogLevel::kFatal, "Invalid device address for LED driver");
-        return std::nullopt;
-    }
+  if (device_address != kDefaultLedDriverAddress) {
+    logger.log(core::LogLevel::kFatal, "Invalid device address for LED driver");
+    return std::nullopt;
+  }
 
-    auto ledDriver = LedDriver(logger, i2c, device_address);
-    if (auto init_result = ledDriver.initialise(); !init_result) {
-        logger.log(core::LogLevel::kFatal, "Failed to initialise LED driver");
-        return std::nullopt;
-    }
+  auto ledDriver = LedDriver(logger, i2c, device_address);
+  if (auto init_result = ledDriver.initialise(); !init_result) {
+    logger.log(core::LogLevel::kFatal, "Failed to initialise LED driver");
+    return std::nullopt;
+  }
 
-    logger.log(core::LogLevel::kDebug, "Successfully initialised LED driver");
-    
-    return ledDriver;
+  logger.log(core::LogLevel::kDebug, "Successfully initialised LED driver");
+
+  return ledDriver;
 }
 
-
-LedDriver::LedDriver(core::ILogger &logger, std::shared_ptr<io::II2c> i2c, const std::uint8_t device_address)
+LedDriver::LedDriver(core::ILogger &logger,
+                     std::shared_ptr<io::II2c> i2c,
+                     const std::uint8_t device_address)
     : logger_(logger),
       i2c_(i2c),
       device_address_(device_address)
@@ -36,52 +37,61 @@ LedDriver::~LedDriver()
 
 std::optional<core::Result> LedDriver::initialise()
 {
-    const auto write_led_control_result = i2c_->writeByteToRegister(device_address_, kLEDControlRegister, 0x00);
+  const auto write_led_control_result
+    = i2c_->writeByteToRegister(device_address_, kLEDControlRegister, 0x00);
 
-    if (write_led_control_result == core::Result::kFailure) {
-        logger_.log(core::LogLevel::kFatal, "Failed to initialise LED driver");
-        return std::nullopt;
-    }
+  if (write_led_control_result == core::Result::kFailure) {
+    logger_.log(core::LogLevel::kFatal, "Failed to initialise LED driver");
+    return std::nullopt;
+  }
 
-    return core::Result::kSuccess;
+  return core::Result::kSuccess;
 }
 
-std::optional<core::Result> LedDriver::set_colour(std::uint8_t channel, std::uint8_t red, std::uint8_t green, std::uint8_t blue)
+std::optional<core::Result> LedDriver::set_colour(std::uint8_t channel,
+                                                  std::uint8_t red,
+                                                  std::uint8_t green,
+                                                  std::uint8_t blue)
 {
-    const auto write_red_result = i2c_->writeByteToRegister(device_address_, kColorRegisterBase + channel, red);
-    const auto write_green_result = i2c_->writeByteToRegister(device_address_, kColorRegisterBase + channel + 1, green);
-    const auto write_blue_result = i2c_->writeByteToRegister(device_address_, kColorRegisterBase + channel + 2, blue);
+  const auto write_red_result
+    = i2c_->writeByteToRegister(device_address_, kColorRegisterBase + channel, red);
+  const auto write_green_result
+    = i2c_->writeByteToRegister(device_address_, kColorRegisterBase + channel + 1, green);
+  const auto write_blue_result
+    = i2c_->writeByteToRegister(device_address_, kColorRegisterBase + channel + 2, blue);
 
-    if (write_red_result == core::Result::kFailure || write_green_result == core::Result::kFailure || write_blue_result == core::Result::kFailure) {
-        logger_.log(core::LogLevel::kFatal, "Failed to set color for LED channel %d", channel);
-        return std::nullopt;
-    }
+  if (write_red_result == core::Result::kFailure || write_green_result == core::Result::kFailure
+      || write_blue_result == core::Result::kFailure) {
+    logger_.log(core::LogLevel::kFatal, "Failed to set color for LED channel %d", channel);
+    return std::nullopt;
+  }
 
-    return core::Result::kSuccess;
+  return core::Result::kSuccess;
 }
 
 std::optional<core::Result> LedDriver::set_intensity(std::uint8_t channel, std::uint8_t intensity)
 {
-    const auto write_intensity_result = i2c_->writeByteToRegister(device_address_, kBrightnessRegisterBase + channel, intensity);
+  const auto write_intensity_result
+    = i2c_->writeByteToRegister(device_address_, kBrightnessRegisterBase + channel, intensity);
 
-    if (write_intensity_result == core::Result::kFailure) {
-        logger_.log(core::LogLevel::kFatal, "Failed to set intensity for LED channel %d", channel);
-        return std::nullopt;
-    }
+  if (write_intensity_result == core::Result::kFailure) {
+    logger_.log(core::LogLevel::kFatal, "Failed to set intensity for LED channel %d", channel);
+    return std::nullopt;
+  }
 
-    return core::Result::kSuccess;
+  return core::Result::kSuccess;
 }
 
 std::optional<core::Result> LedDriver::reset()
 {
-    const auto write_reset_result = i2c_->writeByteToRegister(device_address_, kResetRegister, 0x00);
+  const auto write_reset_result = i2c_->writeByteToRegister(device_address_, kResetRegister, 0x00);
 
-    if (write_reset_result == core::Result::kFailure) {
-        logger_.log(core::LogLevel::kFatal, "Failed to perform a reset for the LED driver");
-        return std::nullopt;
-    }
+  if (write_reset_result == core::Result::kFailure) {
+    logger_.log(core::LogLevel::kFatal, "Failed to perform a reset for the LED driver");
+    return std::nullopt;
+  }
 
-    return core::Result::kSuccess;
+  return core::Result::kSuccess;
 }
 
-}  // namespace hyped::drivers
+}  // namespace hyped::sensors
--- lib/sensors/led_driver.hpp
+++ lib/sensors/led_driver.hpp
@@ -12,31 +12,36 @@
 int kDefaultLedDriverAddress = 0x30;
 
 namespace hyped::sensors {
-    class LedDriver{
-    public:
-        static std::optional<LedDriver> create(core::ILogger &logger,
-                                                std::shared_ptr<io::II2c> i2c,
-                                                const std::uint8_t device_address = kDefaultLedDriverAddress);
-        ~LedDriver();
+class LedDriver {
+ public:
+  static std::optional<LedDriver> create(core::ILogger &logger,
+                                         std::shared_ptr<io::II2c> i2c,
+                                         const std::uint8_t device_address
+                                         = kDefaultLedDriverAddress);
+  ~LedDriver();
 
-        std::optional<core::Result> initialise();
-        std::optional<core::Result> set_colour(std::uint8_t channel, std::uint8_t red, std::uint8_t green, std::uint8_t blue);
-        std::optional<core::Result> set_intensity(std::uint8_t channel, std::uint8_t intensity);
-        std::optional<core::Result> reset();
+  std::optional<core::Result> initialise();
+  std::optional<core::Result> set_colour(std::uint8_t channel,
+                                         std::uint8_t red,
+                                         std::uint8_t green,
+                                         std::uint8_t blue);
+  std::optional<core::Result> set_intensity(std::uint8_t channel, std::uint8_t intensity);
+  std::optional<core::Result> reset();
 
+ private:
+  LedDriver(core::ILogger &logger,
+            std::shared_ptr<io::II2c> i2c,
+            const std::uint8_t device_address);
 
-    private:
-        LedDriver(core::ILogger &logger, std::shared_ptr<io::II2c> i2c, const std::uint8_t device_address);
+ private:
+  core::ILogger &logger_;
+  std::shared_ptr<io::II2c> i2c_;
+  const std::uint8_t device_address_;
 
-    private:
-        core::ILogger &logger_;
-        std::shared_ptr<io::II2c> i2c_;
-        const std::uint8_t device_address_;
-    
-    private:
-        static constexpr std::uint8_t kLEDControlRegister = 0x02;
-        static constexpr std::uint8_t kBrightnessRegisterBase = 0x08;
-        static constexpr std::uint8_t kColorRegisterBase = 0x14;
-        static constexpr std::uint8_t kResetRegister = 0x38;
+ private:
+  static constexpr std::uint8_t kLEDControlRegister     = 0x02;
+  static constexpr std::uint8_t kBrightnessRegisterBase = 0x08;
+  static constexpr std::uint8_t kColorRegisterBase      = 0x14;
+  static constexpr std::uint8_t kResetRegister          = 0x38;
 };
-} // namespace hyped::sensors
+}  // namespace hyped::sensors
