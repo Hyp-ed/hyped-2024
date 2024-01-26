4e8bd0e17c84e94314654a6a31dd958eb372051e
H-Allen <harveyjallen1@gmail.com>
2024-01-26 12:46:03 +0000

    .

--- lib/sensors/low_power_bms.hpp
+++ lib/sensors/low_power_bms.hpp
@@ -1,19 +1,36 @@
 #pragma once
 
-#include "i2c_sensors.hpp"
-
 #include <cstdint>
 #include <memory>
 #include <optional>
 
 #include <core/logger.hpp>
+#include <core/types.hpp>
 #include <io/i2c.hpp>
 
+static constexpr std::uint8_t kDefaultLedDriverAddress = 0x10;
 
-namespace hyped::sensors {
-    class LP_BMS {
-        public:
-            
-    }
+namespace hyped::bms {
 
-} // namespace hyped::sensors
+class LowPowerBMS {
+ public:
+  static std::optional<LowPowerBMS> create(core::ILogger &logger,
+                                           std::shared_ptr<io::II2c> i2c,
+                                           const std::uint8_t device_address);
+  ~LowPowerBMS();
+
+  std::optional<core::BatteryStatus> getBatteryStatus();
+
+ private:
+  LowPowerBMS(core::ILogger &logger, std::shared_ptr<io::II2c> i2c, const std::uint8_t device_address);
+
+ private:
+  core::ILogger &logger_;
+  std::shared_ptr<io::II2c> i2c_;
+  const std::uint8_t device_address_;
+
+  static constexpr std::uint8_t kBatteryStatusRegister = 0x10;
+
+};
+
+}  // namespace hyped::bms
