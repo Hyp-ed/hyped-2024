8ecef4bc3b210fd1f76490dc11e5accbe1e1b963
H-Allen <harveyjallen1@gmail.com>
2024-01-26 10:56:53 +0000

    start

---
+++ lib/sensors/low_power_bms.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "i2c_sensors.hpp"
+
+#include <cstdint>
+#include <memory>
+#include <optional>
+
+#include <core/logger.hpp>
+#include <io/i2c.hpp>
+
+
+namespace hyped::sensors {
+    class LP_BMS {
+        public:
+            
+    }
+
+} // namespace hyped::sensors
