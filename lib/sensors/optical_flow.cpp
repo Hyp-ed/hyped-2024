d2c4bcf65f8af047e4bf69c498c1a4716def27f0
licornes-fluos <54073764+licornes-fluos@users.noreply.github.com>
2024-01-27 13:24:34 +0000

    new branch with everything

---
+++ lib/sensors/optical_flow.cpp
@@ -0,0 +1,58 @@
+#include "optical_flow.hpp"
+
+namespace hyped::sensors {
+
+std::optional<OpticalFlow> OpticalFlow::create(core::ILogger &logger,
+                                               std::shared_ptr<io::ISpi> spi,
+                                               const std::uint8_t channel)
+{
+  // check we are communicating with the correct sensor
+  const std::uint8_t *device_id;
+  spi->read(kDeviceIdAddress, device_id, 1);
+  if (!device_id) {
+    logger.log(core::LogLevel::kFatal, "Failed to read the optical flow device");
+    return std::nullopt;
+  }
+  if (*device_id != kExpectedDeviceIdValue) {
+    logger.log(core::LogLevel::kFatal, "Failure, mismatched device ID for optical flow sensor");
+    return std::nullopt;
+  }
+  return OpticalFlow(logger, spi, channel);
+}
+
+OpticalFlow::OpticalFlow(core::ILogger &logger,
+                         std::shared_ptr<io::ISpi> spi,
+                         const std::uint8_t channel)
+    : logger_(logger),
+      spi_(spi),
+      channel_(channel)
+{
+}
+
+OpticalFlow::~OpticalFlow()
+{
+}
+
+// inspired by Bitcraze_PMW3901 on github
+// (https://github.com/bitcraze/Bitcraze_PMW3901/blob/master/src/Bitcraze_PMW3901.cpp)
+std::uint8_t OpticalFlow::getDeltaX(std::shared_ptr<io::ISpi> spi)
+{
+  std::uint8_t *x_low;
+  std::uint8_t *x_high;
+
+  spi->read(kXLowAddress, x_low, 1);
+  spi->read(kXHighAddress, x_high, 1);
+
+  return static_cast<std::int16_t>((*x_high << 8) | *x_low);
+}
+std::uint8_t OpticalFlow::getDeltaY(std::shared_ptr<io::ISpi> spi)
+{
+  std::uint8_t *y_low;
+  std::uint8_t *y_high;
+
+  spi->read(kYLowAddress, y_low, 1);
+  spi->read(kYHighAddress, y_high, 1);
+
+  return static_cast<std::int16_t>((*y_high << 8) | *y_low);
+}
+}  // namespace hyped::sensors
---
+++ lib/sensors/optical_flow.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <cstdint>
+#include <cstring>
+#include <memory>
+#include <optional>
+
+#include <core/logger.hpp>
+#include <core/types.hpp>
+#include <io/spi.hpp>
+
+namespace hyped::sensors {
+
+class OpticalFlow {
+  // include "magic sauce" optimisation?
+ public:
+  static std::optional<OpticalFlow> create(core::ILogger &logger,
+                                           std::shared_ptr<io::ISpi> spi,
+                                           const std::uint8_t channel);
+  ~OpticalFlow();
+
+ private:
+  OpticalFlow(core::ILogger &logger, std::shared_ptr<io::ISpi> spi, const std::uint8_t channel);
+  std::uint8_t getDeltaX(std::shared_ptr<io::ISpi> spi);
+  std::uint8_t getDeltaY(std::shared_ptr<io::ISpi> spi);
+
+ private:
+  core::ILogger &logger_;
+  std::shared_ptr<io::ISpi> spi_;
+  const std::uint8_t channel_;
+
+ private:
+  // Register addresses for x and y
+  static constexpr std::uint8_t kXLowAddress  = 0x03;
+  static constexpr std::uint8_t kXHighAddress = 0x04;
+  static constexpr std::uint8_t kYLowAddress  = 0x05;
+  static constexpr std::uint8_t kYHighAddress = 0x06;
+
+  static constexpr std::uint8_t kDeviceIdAddress       = 0x00;
+  static constexpr std::uint8_t kExpectedDeviceIdValue = 0x49;  // unsure that's the value
+};
+
+}  // namespace hyped::sensors
