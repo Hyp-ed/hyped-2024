65a954b1845af6d230952630974c3e50d0065e65
Michael Belyaev <michael7belyaev@gmail.com>
2024-01-28 16:05:23 +0000

    formating

--- test/navigation/kalman_filter.cpp
+++ test/navigation/kalman_filter.cpp
@@ -17,15 +17,14 @@ TEST(KalmanFilter, construction)
   navigation::ErrorCovarianceMatrix initial_error_covariance;
   KalmanFilter kalman_filter(
     initial_state,
-    navigation::ErrorCovarianceMatrix::Zero(),  //TODOLater: If initial position not known exactly, tune
+    navigation::ErrorCovarianceMatrix::Zero(),  // TODOLater: If initial position not known exactly,
+                                                // tune
+
     navigation::kStateTransitionMatrix,
     navigation::kControlMatrix,
     navigation::kErrorCovarianceMatrix,
     navigation::measurement_matrix,
     navigation::kMeasurementNoiseCovarianceMatrix);
-
-  
-
 }
 
 }  // namespace hyped::test
