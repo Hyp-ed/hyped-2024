e2ea9d2b6c7420a2e61002abae6db46fb1b6b8d3
H-Allen <harveyjallen1@gmail.com>
2024-02-18 22:49:07 +0000

    .

--- lib/sensors/low_power_bms.cpp
+++ lib/sensors/low_power_bms.cpp
@@ -1,17 +1,21 @@
-#include "low_power_bmse.hpp"
+#include "low_power_bms.hpp"
 
-namespace hyped::bms {
+namespace hyped::sensors {
 
 std::optional<LowPowerBMS> LowPowerBMS::create(core::ILogger &logger,
                                                std::shared_ptr<io::II2c> i2c,
                                                const std::uint8_t device_address)
 {
+  if (device_address != kDefaultLpBmsReadAddress) {
+    logger.log(core::LogLevel::kFatal, "Invalid device address for accelerometer");
+    return std::nullopt;
+  }
   return LowPowerBMS(logger, i2c, device_address);
 }
 
 LowPowerBMS::LowPowerBMS(core::ILogger &logger, std::shared_ptr<io::II2c> i2c, const std::uint8_t device_address)
     : logger_(logger),
-      i2c_(i2c),
+      i2c_(i2c)
 {
 }
 
@@ -19,20 +23,18 @@ LowPowerBMS::~LowPowerBMS()
 {
 }
 
-std::optional<core::BatteryStatus> LowPowerBMS::getBatteryData()
+const u_int32_t LowPowerBMS::getCellData()
 {
-  const auto battery_status = i2c_->readByte(kBatteryStatusRegister);
-
-  if (!battery_status) {
-    logger_.log(core::LogLevel::kError, "Failed to read battery status from Low Power BMS");
-    return std::nullopt;
+  const uint8_t size = sizeof(cell_voltages);
+  const u_int32_t voltages[size] = {};
+  for (int i = 0; i < size; i++) {
+    voltages[i] = i2c_->readByte(kDefaultLpBmsReadAddress, cell_voltages[i]);
   }
-
-  return status;
+  return voltages;l
 }
 
-std:optional<core::BatteryStatus> LowPowerBMS::getCellData() {
-  const auto battery_status = i2c_->readByte()
+const auto LowPowerBMS::getStackVoltage() {
+  const auto stack_voltage = i2c_->readByte(kDefaultLpBmsReadAddress, stack_voltage);
+  return stack_voltage;
 }
-
 }  // namespace hyped::sensors
--- lib/sensors/low_power_bms.hpp
+++ lib/sensors/low_power_bms.hpp
@@ -11,15 +11,16 @@
 static constexpr std::uint8_t kDefaultLpBmsReadAddress = 0x10;
 static constexpr std::uint8_t kDefaultLpBmsWriteAddress = 0x11;
 
-namespace hyped::bms {
+namespace hyped::sensors {
 
 class LowPowerBMS {
  public:
   static std::optional<LowPowerBMS> create(core::ILogger &logger,
-                                           std::shared_ptr<io::II2c> i2c,);
+                                           std::shared_ptr<io::II2c> i2c);
   ~LowPowerBMS();
 
-  std::optional<core::BatteryStatus> getBatteryStatus();
+  std::optional<LowPowerBMS> getStackVoltage();
+  std::optional<LowPowerBMS> getCellData();
 
  private:
   LowPowerBMS(core::ILogger &logger, std::shared_ptr<io::II2c> i2c);
@@ -31,8 +32,6 @@ class LowPowerBMS {
  
  private:
   static constexpr std::uint8_t cell_voltages[] = {0x14, 0x16, 0x18, 0x1A, 0x1C, 0x1E, 0x20, 0x22, 0x24, 0x26, 0x28, 0x2A, 0x2C, 0x2E, 0x30, 0x32};
-  static constexpr std::uint8_t stack_voltage = 0x34
-
+  static constexpr std::uint8_t stack_voltage = 0x34;
 };
-
 }  // namespace hyped::sensors
