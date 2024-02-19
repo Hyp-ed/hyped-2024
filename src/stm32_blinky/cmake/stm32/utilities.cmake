b775edab840d8ee912f11611b6722d8063c57d70
Tom Lonergan <tomlonergan91@gmail.com>
2024-02-19 16:27:41 +0000

    remove linker script, just use files

--- src/stm32_blinky/cmake/stm32/utilities.cmake
+++ src/stm32_blinky/cmake/stm32/utilities.cmake
@@ -49,93 +49,7 @@ endfunction()
 
 include(FetchContent)
 
-set(STM32_FETCH_FAMILIES F4 F7)
-set(STM32_FETCH_CUBE_VERSIONS v1.26.1 v1.16.1)
-set(STM32_FETCH_CMSIS_VERSIONS v2.6.6 v1.2.6)
-set(STM32_FETCH_HAL_VERSIONS v1.7.12 v1.2.9)
-
-FetchContent_Declare(
-  STM32-CMSIS
-  GIT_REPOSITORY https://github.com/STMicroelectronics/cmsis_core/
-  GIT_TAG v5.6.0
-  GIT_PROGRESS TRUE
-)
-
-set(IDX 0)
-foreach(FAMILY ${STM32_FETCH_FAMILIES})
-  string(TOLOWER ${FAMILY} FAMILY_L)
-  list(
-    GET
-    STM32_FETCH_CUBE_VERSIONS
-    ${IDX}
-    CUBE_VERSION
-  )
-  list(
-    GET
-    STM32_FETCH_CMSIS_VERSIONS
-    ${IDX}
-    CMSIS_VERSION
-  )
-  list(
-    GET
-    STM32_FETCH_HAL_VERSIONS
-    ${IDX}
-    HAL_VERSION
-  )
-
-  FetchContent_Declare(
-    STM32Cube${FAMILY}
-    GIT_REPOSITORY https://github.com/STMicroelectronics/STM32Cube${FAMILY}/
-    GIT_TAG ${CUBE_VERSION}
-    GIT_PROGRESS TRUE
-  )
-
-  if(CMSIS_VERSION STREQUAL cube)
-    set(STM32_USE_CMSIS_FROM_CUBE_${FAMILY} ON)
-  else()
-    FetchContent_Declare(
-      STM32-CMSIS-${FAMILY}
-      GIT_REPOSITORY
-        https://github.com/STMicroelectronics/cmsis_device_${FAMILY_L}/
-      GIT_TAG ${CMSIS_VERSION}
-      GIT_PROGRESS TRUE
-    )
-  endif()
-
-  if(HAL_VERSION STREQUAL cube)
-    set(STM32_USE_HAL_FROM_CUBE_${FAMILY} ON)
-  else()
-    FetchContent_Declare(
-      STM32-HAL-${FAMILY}
-      GIT_REPOSITORY
-        https://github.com/STMicroelectronics/stm32${FAMILY_L}xx_hal_driver/
-      GIT_TAG ${HAL_VERSION}
-      GIT_PROGRESS TRUE
-    )
-  endif()
-
-  math(EXPR IDX "${IDX} + 1")
-endforeach()
-
-function(stm32_fetch_cube)
-  foreach(FAMILY ${ARGV})
-    set(CUBE_NAME STM32Cube${FAMILY})
-    string(TOLOWER ${CUBE_NAME} CUBE_NAME_L)
-
-    if(STM32_CUBE_${FAMILY}_PATH)
-      message(
-        VERBOSE
-        "STM32_CUBE_${FAMILY}_PATH specified, skipping fetch for ${CUBE_NAME}"
-      )
-      continue()
-    endif()
-
-    FetchContent_MakeAvailable(${CUBE_NAME})
-    set(STM32_CUBE_${FAMILY}_PATH ${${CUBE_NAME_L}_SOURCE_DIR} PARENT_SCOPE)
-  endforeach()
-endfunction()
-
-function(stm32_fetch_cmsis)
+function(stm32_fetch_cmsis FAMILY)
   if(NOT STM32_CMSIS_PATH)
     FetchContent_MakeAvailable(STM32-CMSIS)
     set(STM32_CMSIS_PATH ${stm32-cmsis_SOURCE_DIR} PARENT_SCOPE)
@@ -143,59 +57,51 @@ function(stm32_fetch_cmsis)
     message(INFO "STM32_CMSIS_PATH specified, skipping fetch for STM32-CMSIS")
   endif()
 
-  foreach(FAMILY ${ARGV})
-    if(STM32_USE_CMSIS_FROM_CUBE_${FAMILY})
-      stm32_fetch_cube(${FAMILY})
+  if(STM32_USE_CMSIS_FROM_CUBE_${FAMILY})
+    stm32_fetch_cube(${FAMILY})
+    message(STATUS "Cube fetched for ${FAMILY} at ${STM32_CUBE_${FAMILY}_PATH}")
+    set(STM32_CMSIS_${FAMILY}_PATH
+        ${STM32_CUBE_${FAMILY}_PATH}/Drivers/CMSIS/Device/ST/STM32${FAMILY}xx
+        PARENT_SCOPE
+    )
+  else()
+    set(CMSIS_NAME STM32-CMSIS-${FAMILY})
+    string(TOLOWER ${CMSIS_NAME} CMSIS_NAME_L)
+
+    if(STM32_CMSIS_${FAMILY}_PATH)
       message(
-        STATUS "Cube fetched for ${FAMILY} at ${STM32_CUBE_${FAMILY}_PATH}"
+        INFO
+        "STM32_CMSIS_${FAMILY}_PATH specified, skipping fetch for ${CMSIS_NAME}"
       )
-      set(STM32_CMSIS_${FAMILY}_PATH
-          ${STM32_CUBE_${FAMILY}_PATH}/Drivers/CMSIS/Device/ST/STM32${FAMILY}xx
-          PARENT_SCOPE
-      )
-    else()
-      set(CMSIS_NAME STM32-CMSIS-${FAMILY})
-      string(TOLOWER ${CMSIS_NAME} CMSIS_NAME_L)
-
-      if(STM32_CMSIS_${FAMILY}_PATH)
-        message(
-          INFO
-          "STM32_CMSIS_${FAMILY}_PATH specified, skipping fetch for ${CMSIS_NAME}"
-        )
-        continue()
-      endif()
-
-      FetchContent_MakeAvailable(${CMSIS_NAME})
-      set(STM32_CMSIS_${FAMILY}_PATH ${${CMSIS_NAME_L}_SOURCE_DIR} PARENT_SCOPE)
+      continue()
     endif()
-  endforeach()
+
+    FetchContent_MakeAvailable(${CMSIS_NAME})
+    set(STM32_CMSIS_${FAMILY}_PATH ${${CMSIS_NAME_L}_SOURCE_DIR} PARENT_SCOPE)
+  endif()
 endfunction()
 
-function(stm32_fetch_hal)
-  foreach(FAMILY ${ARGV})
-    if(STM32_USE_HAL_FROM_CUBE_${FAMILY})
-      stm32_fetch_cube(${FAMILY})
+function(stm32_fetch_hal FAMILY)
+  if(STM32_USE_HAL_FROM_CUBE_${FAMILY})
+    stm32_fetch_cube(${FAMILY})
+    message(STATUS "Cube fetched for ${FAMILY} at ${STM32_CUBE_${FAMILY}_PATH}")
+    set(STM32_HAL_${FAMILY}_PATH
+        ${STM32_CUBE_${FAMILY}_PATH}/Drivers/STM32${FAMILY}xx_HAL_Driver
+        PARENT_SCOPE
+    )
+  else()
+    set(HAL_NAME STM32-HAL-${FAMILY})
+    string(TOLOWER ${HAL_NAME} HAL_NAME_L)
+
+    if(STM32_HAL_${FAMILY}_PATH)
       message(
-        STATUS "Cube fetched for ${FAMILY} at ${STM32_CUBE_${FAMILY}_PATH}"
+        INFO
+        "STM32_HAL_${FAMILY}_PATH specified, skipping fetch for ${HAL_NAME}"
       )
-      set(STM32_HAL_${FAMILY}_PATH
-          ${STM32_CUBE_${FAMILY}_PATH}/Drivers/STM32${FAMILY}xx_HAL_Driver
-          PARENT_SCOPE
-      )
-    else()
-      set(HAL_NAME STM32-HAL-${FAMILY})
-      string(TOLOWER ${HAL_NAME} HAL_NAME_L)
-
-      if(STM32_HAL_${FAMILY}_PATH)
-        message(
-          INFO
-          "STM32_HAL_${FAMILY}_PATH specified, skipping fetch for ${HAL_NAME}"
-        )
-        continue()
-      endif()
-
-      FetchContent_MakeAvailable(${HAL_NAME})
-      set(STM32_HAL_${FAMILY}_PATH ${${HAL_NAME_L}_SOURCE_DIR} PARENT_SCOPE)
+      continue()
     endif()
-  endforeach()
+
+    FetchContent_MakeAvailable(${HAL_NAME})
+    set(STM32_HAL_${FAMILY}_PATH ${${HAL_NAME_L}_SOURCE_DIR} PARENT_SCOPE)
+  endif()
 endfunction()
