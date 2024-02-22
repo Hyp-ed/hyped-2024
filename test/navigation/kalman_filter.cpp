e2e13142c420d741faa3ddd41ba41d52b08094ab
Michael Belyaev <michael7belyaev@gmail.com>
2024-02-22 19:19:41 +0000

    changed kalman filter logic, most importantly ADDED COVARIANCE UPDATE!!!

--- test/navigation/kalman_filter.cpp
+++ test/navigation/kalman_filter.cpp
@@ -9,37 +9,63 @@
 
 namespace hyped::test {
 
+
+//Done for delta_t = 1
+
 TEST(KalmanFilter, construction)
 {
   using KalmanFilter     = navigation::KalmanFilter;
   const auto manual_time = std::make_shared<utils::ManualTime>();
-  navigation::StateVector initial_state(navigation::StateVector::Zero());
-  navigation::ErrorCovarianceMatrix initial_error_covariance;
-  KalmanFilter kalman_filter(
-    initial_state,
-    navigation::ErrorCovarianceMatrix::Zero(),  // TODOLater: If initial position not known exactly,
-                                                // tune
 
-    navigation::kStateTransitionMatrix,
-    navigation::kControlMatrix,
-    navigation::kErrorCovarianceMatrix,
-    navigation::measurement_matrix,
-    navigation::kMeasurementNoiseCovarianceMatrix);
+
+  navigation::StateVector initial_state(navigation::StateVector::Zero());
+
+  navigation::ErrorCovarianceMatrix initial_error_covariance;
+  initial_error_covariance << 400, 0, 0, 400;
+
+  navigation::StateTransitionMatrix transition_matrix;
+  transition_matrix << 1, 1, 0, 1;
+
+  navigation::ControlMatrix control_matrix;
+  control_matrix << 0.5, 1;
+
+  navigation::MeasurementNoiseCovarianceMatrix process_noise_covariance;
+  process_noise_covariance << 1/4, 1/2, 1/2, 1;
+
+  navigation::MeasurementMatrix measurement_matrix;
+  measurement_matrix << 1, 0, 0, 1;
+
+  navigation::MeasurementNoiseCovarianceMatrix measurement_noise_covariance;
+  measurement_noise_covariance << 0, 0, 0, 1;
+
+  KalmanFilter kalman_filter(initial_state,
+                             initial_error_covariance,
+                             transition_matrix,
+                             control_matrix,
+                             process_noise_covariance,
+                             measurement_matrix,
+                             measurement_noise_covariance);
 
   navigation::MeasurementVector measurement;
-  measurement << 1, 1;
   navigation::ControlInput control_input(navigation::ControlInput::Zero());
 
-  // TODOLater: dont be lazy, do the maths and check!!
 
-  for (int i = 0; i < 10; ++i) {
-    measurement << 1 + i, 1;
-    kalman_filter.filter(measurement, control_input);
+  navigation::StateVector state_estimate;
 
-    navigation::StateVector state_estimate = kalman_filter.getStateEstimate();
+  state_estimate = kalman_filter.getStateEstimate();
 
-    std::cout << "State estimate: " << state_estimate << std::endl;
-  }
+  std::cout << "State estimate: " << state_estimate << std::endl;
+
+  measurement << 1, 0;
+
+  
+  kalman_filter.filter(measurement, control_input);
+  
+
+  state_estimate = kalman_filter.getStateEstimate();
+
+  std::cout << "State estimate: " << state_estimate << std::endl;
 }
-
 }  // namespace hyped::test
+
+// namespace hyped::test
