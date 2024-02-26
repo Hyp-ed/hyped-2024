6e5c269c61bfcbec530cb27f821b4508e8614c79
H-Allen <harveyjallen1@gmail.com>
2024-02-26 15:31:21 +0000

    Added more checks. Fixed Bugs.

--- lib/sensors/low_power_bms.cpp
+++ lib/sensors/low_power_bms.cpp
@@ -6,8 +6,13 @@ std::optional<LowPowerBMS> LowPowerBMS::create(core::ILogger &logger,
                                                std::shared_ptr<io::II2c> i2c,
                                                const std::uint8_t device_address)
 {
-  if (device_address != kDefaultLpBmsReadAddress) {
-    logger.log(core::LogLevel::kFatal, "Invalid device address for accelerometer");
+  if (device_address != kDefaultLpBmsAddress) {
+    logger.log(core::LogLevel::kFatal, "Invalid device address for BMS");
+    return std::nullopt;
+  }
+  const auto test = i2c->readByte(device_address, stack_voltage);
+  if (!test) {
+    logger.log(core::LogLevel::kFatal, "Failed to read values from BMS");
     return std::nullopt;
   }
   return LowPowerBMS(logger, i2c, device_address);
@@ -17,7 +22,8 @@ LowPowerBMS::LowPowerBMS(core::ILogger &logger,
                          std::shared_ptr<io::II2c> i2c,
                          const std::uint8_t device_address)
     : logger_(logger),
-      i2c_(i2c)
+      i2c_(i2c),
+      device_address_(device_address)
 {
 }
 
@@ -30,14 +36,14 @@ std::optional<std::uint8_t> LowPowerBMS::getCellData()
   const std::uint8_t size = sizeof(cell_voltages);
   std::optional<std::uint8_t> voltages[size];
   for (int i = 0; i < size; i++) {
-    voltages[i] = i2c_->readByte(kDefaultLpBmsReadAddress, cell_voltages[i]);
+    voltages[i] = i2c_->readByte(device_address_, cell_voltages[i]);
   }
   return *voltages;
 }
 
 std::optional<std::uint8_t> LowPowerBMS::getStackVoltage()
 {
-  const auto voltage = i2c_->readByte(kDefaultLpBmsReadAddress, stack_voltage);
+  const auto voltage = i2c_->readByte(device_address_, stack_voltage);
   return voltage;
 }
 }  // namespace hyped::sensors
--- lib/sensors/low_power_bms.hpp
+++ lib/sensors/low_power_bms.hpp
@@ -8,8 +8,7 @@
 #include <core/types.hpp>
 #include <io/i2c.hpp>
 
-static constexpr std::uint8_t kDefaultLpBmsReadAddress  = 0x10;
-static constexpr std::uint8_t kDefaultLpBmsWriteAddress = 0x11;
+static constexpr std::uint8_t kDefaultLpBmsAddress  = 0x10;
 
 namespace hyped::sensors {
 
@@ -29,13 +28,14 @@ class LowPowerBMS {
               const std::uint8_t device_address);
   core::ILogger &logger_;
   std::shared_ptr<io::II2c> i2c_;
-
-  const std::uint8_t device_read_address  = kDefaultLpBmsReadAddress;
-  const std::uint8_t device_write_address = kDefaultLpBmsWriteAddress;
+  const std::uint8_t device_address_;
 
  private:
   static constexpr std::uint8_t cell_voltages[] = {
-    0x14, 0x16, 0x18, 0x1A, 0x1C, 0x1E, 0x20, 0x22, 0x24, 0x26, 0x28, 0x2A, 0x2C, 0x2E, 0x30, 0x32};
+    0x14, 0x16, 0x18, 0x1A, 
+    0x1C, 0x1E, 0x20, 0x22, 
+    0x24, 0x26, 0x28, 0x2A, 
+    0x2C, 0x2E, 0x30, 0x32};
   static constexpr std::uint8_t stack_voltage = 0x34;
 };
 }  // namespace hyped::sensors
