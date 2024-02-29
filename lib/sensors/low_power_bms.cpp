64b0328248f62fdba4a4d2087223119fdf2467af
H-Allen <harveyjallen1@gmail.com>
2024-02-29 19:20:14 +0000

    Change requests + undervoltage protection

--- lib/sensors/low_power_bms.cpp
+++ lib/sensors/low_power_bms.cpp
@@ -6,11 +6,7 @@ std::optional<LowPowerBMS> LowPowerBMS::create(core::ILogger &logger,
                                                std::shared_ptr<io::II2c> i2c,
                                                const std::uint8_t device_address)
 {
-  if (device_address != kDefaultLpBmsAddress) {
-    logger.log(core::LogLevel::kFatal, "Invalid device address for BMS");
-    return std::nullopt;
-  }
-  const auto test = i2c->readByte(device_address, stack_voltage);
+  const auto test = i2c->readByte(device_address, kStackVoltage);
   if (!test) {
     logger.log(core::LogLevel::kFatal, "Failed to read values from BMS");
     return std::nullopt;
@@ -31,15 +27,12 @@ LowPowerBMS::~LowPowerBMS()
 {
 }
 
-std::optional<std::array<std::uint8_t, 16>> LowPowerBMS::getCellData()
+std::optional<std::array<std::uint8_t, kNumCells>> LowPowerBMS::getCellData()
 {
-  const std::uint8_t size = cells;
-  std::array<std::uint8_t, size> voltages;
-  for (int i = 0; i < size; i++) {
-    const auto voltage = i2c_->readByte(device_address_, cell_voltages[i]);
-    if (!voltage) {
-      return std::nullopt;
-    }
+  std::array<std::uint8_t, kNumCells> voltages;
+  for (auto i = 0; i < kNumCells; i++) {
+    const auto voltage = i2c_->readByte(device_address_, kCellVoltageRegisters[i]);
+    if (!voltage) { return std::nullopt; }
     voltages[i] = *voltage;
   }
   return voltages;
@@ -47,10 +40,17 @@ std::optional<std::array<std::uint8_t, 16>> LowPowerBMS::getCellData()
 
 std::optional<std::uint8_t> LowPowerBMS::getStackVoltage()
 {
-  const auto voltage = i2c_->readByte(device_address_, stack_voltage);
-  if (!voltage) {
-      return std::nullopt;
-  }
+  const auto voltage = i2c_->readByte(device_address_, kStackVoltage);
+  if (!voltage) { return std::nullopt; }
   return voltage;
 }
+
+std::optional<bool> LowPowerBMS::checkUndervoltage()
+{
+  const auto status = i2c_->readByte(device_address_, kUnderVoltage);
+  if (!status) { return std::nullopt; }
+  if (*status & 0b00000100) { return true; }
+  return false;
+}
+
 }  // namespace hyped::sensors
--- lib/sensors/low_power_bms.hpp
+++ lib/sensors/low_power_bms.hpp
@@ -8,9 +8,10 @@
 #include <core/types.hpp>
 #include <io/i2c.hpp>
 
-static constexpr std::uint8_t kDefaultLpBmsAddress  = 0x10;
-
 namespace hyped::sensors {
+
+static constexpr std::uint8_t kDefaultLpBmsAddress = 0x10;
+static constexpr std::uint8_t kNumCells = 16;
 
 class LowPowerBMS {
  public:
@@ -20,7 +21,8 @@ class LowPowerBMS {
   ~LowPowerBMS();
 
   std::optional<std::uint8_t> getStackVoltage();
-  std::optional<std::array<std::uint8_t, 16>> getCellData();
+  std::optional<std::array<std::uint8_t, kNumCells>> getCellData();
+  std::optional<bool> checkUndervoltage();
 
  private:
   LowPowerBMS(core::ILogger &logger,
@@ -31,12 +33,9 @@ class LowPowerBMS {
   const std::uint8_t device_address_;
 
  private:
-  static constexpr std::uint8_t cell_voltages[] = {
-    0x14, 0x16, 0x18, 0x1A, 
-    0x1C, 0x1E, 0x20, 0x22, 
-    0x24, 0x26, 0x28, 0x2A, 
-    0x2C, 0x2E, 0x30, 0x32};
-  static constexpr std::uint8_t cells = sizeof(cell_voltages);
-  static constexpr std::uint8_t stack_voltage = 0x34;
+  static constexpr std::uint8_t kCellVoltageRegisters[] = {
+    0x14, 0x16, 0x18, 0x1A, 0x1C, 0x1E, 0x20, 0x22, 0x24, 0x26, 0x28, 0x2A, 0x2C, 0x2E, 0x30, 0x32};
+  static constexpr std::uint8_t kStackVoltage = 0x34;
+  static constexpr std::uint8_t kUnderVoltage = 0x02;
 };
 }  // namespace hyped::sensors
