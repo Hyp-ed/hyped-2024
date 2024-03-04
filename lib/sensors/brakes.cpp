04feb54be02dd8cce8d64e87e0822359119b54f5
veronicaahn <teulebit.ahn@vanderbilt.edu>
2024-03-04 19:00:16 +0000

    changed new pin to pin

--- lib/sensors/brakes.cpp
+++ lib/sensors/brakes.cpp
@@ -4,9 +4,9 @@ namespace hyped::sensors {
 
 std::optional<Brakes> Brakes::create(core::ILogger &logger,
                                      std::shared_ptr<io::IGpio> gpio,
-                                     const std::uint8_t new_pin)
+                                     const std::uint8_t pin)
 {
-  const auto reader = gpio->getReader(new_pin, io::Edge::kNone);
+  const auto reader = gpio->getReader(pin, io::Edge::kNone);
   if (!reader) {
     logger.log(core::LogLevel::kFatal, "Failed to create Brakes instance");
     return std::nullopt;
