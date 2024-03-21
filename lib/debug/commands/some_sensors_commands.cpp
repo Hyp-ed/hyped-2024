2649f71394438a008fd844ebd93fb3282fa48ddd
licornes-fluos <54073764+licornes-fluos@users.noreply.github.com>
2024-03-21 15:11:53 +0000

    debug toml and some of commands

---
+++ lib/debug/commands/some_sensors_commands.cpp
@@ -0,0 +1,11 @@
+#include "some_sensors_commands.hpp"
+
+namespace hyped::debug {
+core::Result SomeSensorsCommands::addCommands(core::ILogger &logger,
+														 std::shared_ptr<Repl> repl,
+														 toml::v3::node_view<toml::v3::node> config)
+{
+
+}
+
+} // namespace hyped::debug
