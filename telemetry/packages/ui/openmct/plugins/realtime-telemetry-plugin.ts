858f6515bdf46c8ddb01b11baa99e5fec987b8ec
David Beechey <david@dtbeechey.dev>
2024-03-22 16:30:33 +0000

    TEL - Open MCT fixes/updates (#85)

--- telemetry/packages/ui/openmct/plugins/realtime-telemetry-plugin.ts
+++ telemetry/packages/ui/openmct/plugins/realtime-telemetry-plugin.ts
@@ -3,26 +3,21 @@ import { io } from 'socket.io-client';
 import { SERVER_ENDPOINT } from '../core/config';
 import { AugmentedDomainObject } from '../types/AugmentedDomainObject';
 import { socket as socketConstants } from '@hyped/telemetry-constants';
+import { MeasurementReading } from 'openmct/types/MeasurementReading';
+import { RealtimeTelemetryListenerCallback } from 'openmct/types/RealtimeTelemetryListenerCallback';
 
-type MeasurementReading = {
-  podId: string;
-  measurementKey: string;
-  value: number;
-  timestamp: number;
-};
-
+/**
+ * The Realtime Telemetry plugin for Open MCT.
+ * Provides realtime telemetry data by subscribing to the telemetry server using a websocket.
+ * @see https://github.com/nasa/openmct/blob/master/API.md#telemetry-api
+ * @returns The realtime telemetry plugin function.
+ */
 export function RealtimeTelemetryPlugin() {
   return function install(openmct: OpenMCT) {
     const socket = io(SERVER_ENDPOINT, { path: '/openmct/data/realtime' });
-    // handle socket disconnects
 
-    const listenerCallbacks: {
-      [key: string]: (data: {
-        id: string;
-        value: number;
-        timestamp: number;
-      }) => void;
-    } = {};
+    const listenerCallbacks: Record<string, RealtimeTelemetryListenerCallback> =
+      {};
 
     socket.on(socketConstants.MEASUREMENT_EVENT, (data: MeasurementReading) => {
       const { podId, measurementKey, value, timestamp } = data;
@@ -31,15 +26,24 @@ export function RealtimeTelemetryPlugin() {
         measurementKey,
       );
 
+      // When we get a new measurement reading, call the callback for that measurement
       if (listenerCallbacks[roomName]) {
         listenerCallbacks[roomName]({ id: measurementKey, value, timestamp });
       }
     });
 
     const provider = {
+      // We only support realtime telemetry for domain objects that have a podId
       supportsSubscribe: (domainObject: AugmentedDomainObject) => {
         return domainObject.podId !== undefined;
       },
+      /**
+       * Subscribes to realtime telemetry data for a domain object.
+       * Sends a subscription request to the server and saves the callback to `listenerCallbacks`.
+       * @param domainObject The domain object to subscribe to.
+       * @param callback The callback to call when new telemetry data is received.
+       * @returns A function to unsubscribe from the telemetry data.
+       */
       subscribe: (
         domainObject: AugmentedDomainObject,
         callback: () => void,
@@ -50,6 +54,7 @@ export function RealtimeTelemetryPlugin() {
           identifier.key,
         );
 
+        // Save the callback to call when new telemetry data is received
         listenerCallbacks[roomName] = callback;
         socket.emit(socketConstants.EVENTS.SUBSCRIBE_TO_MEASUREMENT, roomName);
 
