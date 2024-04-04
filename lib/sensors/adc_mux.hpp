fe7dda8d021dfc274446707696c92763039efdfe
H-Allen <harveyjallen1@gmail.com>
2024-04-04 19:08:08 +0100

    format

--- lib/sensors/adc_mux.hpp
+++ lib/sensors/adc_mux.hpp
@@ -6,14 +6,15 @@
 #include <core/types.hpp>
 
 namespace hyped::sensors {
-    constexpr kAdcMuxAddress = 0x1D;
-    
-    constexpr kAdcMuxChannel1 = 0x20;
-    constexpr kAdcMuxChannel2 = 0x21;
-    constexpr kAdcMuxChannel3 = 0x22;
-    constexpr kAdcMuxChannel4 = 0x23;
-    constexpr kAdcMuxChannel5 = 0x24;
-    constexpr kAdcMuxChannel6 = 0x25;
-    constexpr kAdcMuxChannel7 = 0x26;
-    constexpr kAdcMuxChannel0 = 0x27;
-}
+
+constexpr kAdcMuxAddress = 0x1D;
+constexpr kAdcMuxChannel1 = 0x20;
+constexpr kAdcMuxChannel2 = 0x21;
+constexpr kAdcMuxChannel3 = 0x22;
+constexpr kAdcMuxChannel4 = 0x23;
+constexpr kAdcMuxChannel5 = 0x24;
+constexpr kAdcMuxChannel6 = 0x25;
+constexpr kAdcMuxChannel7 = 0x26;
+constexpr kAdcMuxChannel0 = 0x27;
+
+}  // namespace hyped::sensors
