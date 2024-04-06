05e1ed6e54a967f769d36067d7b66d0f792544fa
David Beechey <david@dtbeechey.dev>
2024-04-06 14:31:11 +0100

    fix header file

--- lib/sensors/inverter_current.hpp
+++ lib/sensors/inverter_current.hpp
@@ -12,17 +12,13 @@ namespace hyped::sensors {
 
 class InverterCurrent {
  public:
-  static std::optional<InverterCurrent> create(core::ILogger &logger,
-                                               std::shared_ptr<io::II2c> i2c,
-                                               const std::uint8_t adc_mux_channel);
-  ~InverterCurrent();
-
-  std::optional<core::Float> readCurrent();
-
- private:
   InverterCurrent(core::ILogger &logger,
                   std::shared_ptr<io::II2c> i2c,
                   const std::uint8_t adc_mux_channel);
+
+  ~InverterCurrent();
+
+  std::optional<core::Float> readCurrent();
 
  private:
   core::ILogger &logger_;
