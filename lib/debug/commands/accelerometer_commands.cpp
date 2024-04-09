201a481e2e3ea2ce98665b59c78b04707cd8d547
licornes-fluos <54073764+licornes-fluos@users.noreply.github.com>
2024-04-09 18:13:02 +0100

    rename branch and part of testing code

---
+++ lib/debug/commands/accelerometer_commands.cpp
@@ -0,0 +1,44 @@
+#include "accelerometer_commands.hpp"
+
+#include <cstdint>
+
+namespace hyped::debug {
+core::Result AccelerometerCommands::addCommands(core::ILogger &logger,
+														 std::shared_ptr<Repl> repl,
+														 toml::v3::node_view<toml::v3::node> config)
+{
+	// get bus
+	const auto optional_bus = config["bus"].value<std::uint8_t>();
+  	if (!optional_bus) {
+    	logger.log(core::LogLevel::kFatal, "No I2C bus specified");
+    	return core::Result::kFailure;
+	};
+	const auto bus = *optional_bus;
+
+	// get I2C instance
+	const auto optional_i2c = repl->getI2c(bus);
+    if (!optional_i2c) {
+      logger.log(core::LogLevel::kFatal, "Error creating I2C bus");
+      return core::Result::kFailure;
+    };
+	const auto i2c = std::move(*optional_i2c);
+
+	// instance of sensor
+	const auto accelerometer_sensor 			= std::make_shared<sensors::Accelerometer>(logger, i2c);
+	const auto accelerometer_read 				= "read";
+	const auto accelerometer_read_description 	= "Read accelerometer";
+	const auto accelerometer_read_handler 		= [&logger, accelerometer_sensor]() {
+		const auto value_ready = accelerometer_sensor -> isValueReady(); // method already returns state
+		const auto acceleration = accelerometer_sensor -> read();
+		if (!acceleration) {
+			logger.log(core::LogLevel::kFatal, "Failed to read acceleration");
+			return;
+		}
+		logger.log(core::LogLevel::kDebug, "Acceleration: %d", *acceleration);
+
+	};
+	
+
+
+}
+} // namespace hyped::debug
---
+++ lib/debug/commands/accelerometer_commands.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "command.hpp"
+
+#include <memory>
+
+#include <core/logger.hpp>
+#include <core/types.hpp>
+#include <debug/repl.hpp>
+#include <sensors/accelerometer.hpp>
+
+namespace hyped::debug {
+
+class AccelerometerCommands {
+ public:
+  static core::Result addCommands(core::ILogger &logger,
+                                  			 std::shared_ptr<Repl> repl,
+                                  			 toml::v3::node_view<toml::v3::node> config);
+};
+
+}  // namespace hyped::debug
