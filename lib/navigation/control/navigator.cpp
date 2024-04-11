8f1f79dae7371c5be2bf4fe3b6c66457b0314701
Michael Belyaev <michael7belyaev@gmail.com>
2024-04-11 18:33:52 +0100

    F-f-f-formatting

--- lib/navigation/control/navigator.cpp
+++ lib/navigation/control/navigator.cpp
@@ -14,7 +14,7 @@ Navigator::Navigator(core::ILogger &logger, const core::ITimeSource &time)
       previous_accelerometer_data_(0.0),
       previous_optical_reading_(0.0),
       previous_keyence_reading_(0.0),
-      kalman_filter_(initial_state, 
+      kalman_filter_(initial_state,
                      initial_covariance,
                      kStateTransitionMatrix,
                      kControlMatrix,
