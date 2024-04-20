b98b75b7e3e477149ac6e1a45d0c21ff9d895a00
Conrad Thomson <s2076431@ed.ac.uk>
2024-04-20 05:12:09 +0100

    commit changes to set up new branches for other tests

--- telemetry/packages/e2e-tests/tests/sidebar.spec.ts
+++ telemetry/packages/e2e-tests/tests/sidebar.spec.ts
@@ -1,4 +1,6 @@
 import { test, expect } from '@playwright/test';
+import { validateMqttMessage } from '../lib/mqtt';
+
 import { levitationHeightCommon } from '@hyped/telemetry-constants/src/pods/common';
 const  { limits: { critical } } = levitationHeightCommon;
 
@@ -8,11 +10,22 @@ test('registers user input for levitation height input', async ({ page }) => {
   // Manouvre to debug logging view
   await page.click('text="Logs"');
 
+  await validateMqttMessage(
+    () => {},
+    (receivedTopic, msg) => {
+      const connectionStatus = msg.toString();
+      if (connectionStatus !== 'Connected') {
+        throw new Error(`Connection status is not established, got ${connectionStatus} waiting...`);
+      }
+    },
+    5000, // timeout after excessive load time
+  );
+  
   // Ensure connection is established
-  await expect(page.locator)
-
+  expect(['Connected, Disconnected']).toContain(connectionStatus);
+  
   const [lower, upper] = [critical.low, critical.high];
-  // Input random value within allowable range
+  // Input random value within allowable ranged
   const testInput = Math.floor(Math.random() * (upper - lower)).toString();
   await page.fill('[data-testid="height-input"]', testInput);
   // Set height and send levitation message to control system
@@ -22,7 +35,7 @@ test('registers user input for levitation height input', async ({ page }) => {
   await expect(page).
 })
 
-/**
+/**]     ß
  * Tests response for input within allowed range of levitation height
  */
 // test('correct response for allowed height input value', asymc ({ page }) => {
