3531cf7befbdb7c561cc9087387030e7a7f5baa9
licornes-fluos <54073764+licornes-fluos@users.noreply.github.com>
2024-05-05 21:30:07 +0100

    changes to test code

--- lib/debug/commands/accelerometer_commands.cpp
+++ lib/debug/commands/accelerometer_commands.cpp
@@ -41,21 +41,23 @@ core::Result AccelerometerCommands::addCommands(core::ILogger &logger,
   auto accelerometer_sensor           = std::move(*optional_accelerometer_sensor);
   const auto read_command_name        = "accelerometer read";
   const auto read_command_description = "Read from the accelerometer";
-  const auto read_command_handler     = [&logger, &accelerometer_sensor]() {
-    const auto value_ready = accelerometer_sensor.isValueReady();
-    if (!value_ready) {
-      logger.log(core::LogLevel::kFatal, "Value is not ready");
-      return;
-    }
-    const auto acceleration = accelerometer_sensor.read();
-    if (!acceleration) {
-      logger.log(core::LogLevel::kFatal, "Failed to read acceleration");
-      return;
-    }
-    logger.log(core::LogLevel::kDebug, "Acceleration: %s m/s", acceleration);
-  };
-  auto read_command
-    = std::make_unique<Command>(read_command_name, read_command_description, read_command_handler);
+  const auto read_command_usage       = "accelerometer read";
+  const auto read_command_handler
+    = [&logger, &accelerometer_sensor](const std::vector<std::string> &) {
+        const auto value_ready = accelerometer_sensor.isValueReady();
+        if (!value_ready) {
+          logger.log(core::LogLevel::kFatal, "Value is not ready");
+          return;
+        }
+        const auto acceleration = accelerometer_sensor.read();
+        if (!acceleration) {
+          logger.log(core::LogLevel::kFatal, "Failed to read acceleration");
+          return;
+        }
+        logger.log(core::LogLevel::kDebug, "Acceleration: %s m/s", acceleration);
+      };
+  auto read_command = std::make_unique<Command>(
+    read_command_name, read_command_description, read_command_usage, read_command_handler);
   repl->addCommand(std::move(read_command));
   return core::Result::kSuccess;
 }
