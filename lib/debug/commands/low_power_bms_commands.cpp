9869f2b9ec02f7729460321ba0a47f2ce0e8189f
licornes-fluos <54073764+licornes-fluos@users.noreply.github.com>
2024-05-05 21:30:34 +0100

    clang-format

--- lib/debug/commands/low_power_bms_commands.cpp
+++ lib/debug/commands/low_power_bms_commands.cpp
@@ -42,31 +42,33 @@ core::Result LowPowerBMSCommands::addCommands(core::ILogger &logger,
   const auto read_command_name        = "low power bms read";
   const auto read_command_description = "Read from the low power bms";
   const auto read_command_usage       = "low power bms read";
-  const auto read_command_handler     = [&logger, &low_power_bms_sensor](const std::vector<std::string> &) {
-    // const std::array<std::uint8_t, sensors::kNumCells> cell_data[] =
-    // low_power_bms_sensor.getCellData();
-    const auto cell_data = low_power_bms_sensor.getCellData();
-    if (!cell_data) {
-      logger.log(core::LogLevel::kFatal, "Failed to get cell data");
-      return;
-    }
-    // for (auto i = 0; i < sensors::kNumCells; i++){
-    // 	logger.log(core::LogLevel::kDebug, "Cell data at %d is %u ", i, cell_data[i]);
-    // }
-    logger.log(core::LogLevel::kDebug, "Cell data: %u", cell_data);
-    const auto stack_voltage = low_power_bms_sensor.getStackVoltage();
-    if (!stack_voltage) {
-      logger.log(core::LogLevel::kFatal, "Failed to get stack voltage");
-      return;
-    }
-    logger.log(core::LogLevel::kDebug, "Stack voltage: %u V", stack_voltage);
-    const auto undervoltage = low_power_bms_sensor.checkUndervoltage();
-    if (!undervoltage) { logger.log(core::LogLevel::kFatal, "Failed to check for undervoltage"); }
-    logger.log(core::LogLevel::kDebug, "Undervoltage: %s", undervoltage ? "true" : "false");
-  };
-  auto read_command
-    = std::make_unique<Command>(
-      read_command_name, read_command_description, read_command_usage, read_command_handler);
+  const auto read_command_handler
+    = [&logger, &low_power_bms_sensor](const std::vector<std::string> &) {
+        // const std::array<std::uint8_t, sensors::kNumCells> cell_data[] =
+        // low_power_bms_sensor.getCellData();
+        const auto cell_data = low_power_bms_sensor.getCellData();
+        if (!cell_data) {
+          logger.log(core::LogLevel::kFatal, "Failed to get cell data");
+          return;
+        }
+        // for (auto i = 0; i < sensors::kNumCells; i++){
+        // 	logger.log(core::LogLevel::kDebug, "Cell data at %d is %u ", i, cell_data[i]);
+        // }
+        logger.log(core::LogLevel::kDebug, "Cell data: %u", cell_data);
+        const auto stack_voltage = low_power_bms_sensor.getStackVoltage();
+        if (!stack_voltage) {
+          logger.log(core::LogLevel::kFatal, "Failed to get stack voltage");
+          return;
+        }
+        logger.log(core::LogLevel::kDebug, "Stack voltage: %u V", stack_voltage);
+        const auto undervoltage = low_power_bms_sensor.checkUndervoltage();
+        if (!undervoltage) {
+          logger.log(core::LogLevel::kFatal, "Failed to check for undervoltage");
+        }
+        logger.log(core::LogLevel::kDebug, "Undervoltage: %s", undervoltage ? "true" : "false");
+      };
+  auto read_command = std::make_unique<Command>(
+    read_command_name, read_command_description, read_command_usage, read_command_handler);
   repl->addCommand(std::move(read_command));
   return core::Result::kSuccess;
 };
