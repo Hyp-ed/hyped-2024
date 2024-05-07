7ebe7ab588f3bc9bb8b7814b848c7046ad3d573b
Tom Lonergan <tomlonergan91@gmail.com>
2024-05-07 19:51:03 +0100

    rewrite i2c mux

--- test/sensors/i2c_mux.cpp
+++ test/sensors/i2c_mux.cpp
@@ -12,14 +12,9 @@ namespace hyped::test {
 
 TEST(I2cMux, construction)
 {
-  static constexpr std::uint8_t kSize = 8;
-  std::array<std::unique_ptr<sensors::II2cMuxSensor<std::uint8_t>>, kSize> sensors;
-  for (auto &sensor : sensors) {
-    sensor = std::make_unique<utils::DummyI2cSensor>();
-  }
   const auto i2c = std::make_shared<utils::DummyI2c>();
   utils::DummyLogger logger;
-  sensors::I2cMux<std::uint8_t, kSize> mux(logger, i2c, 0, sensors);
+  sensors::I2cMux mux(logger, i2c, sensors::kDefaultMuxAddress, 0);
 }
 
 }  // namespace hyped::test
