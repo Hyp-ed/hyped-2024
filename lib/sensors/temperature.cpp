1ae17f05a9a4503fec299c392c57ee63b97fb939
Tom Lonergan <43603330+TomLonergan03@users.noreply.github.com>
2024-05-13 18:34:38 +0100

    SNS - Temperature sensor REPL commands (#99)

--- lib/sensors/temperature.cpp
+++ lib/sensors/temperature.cpp
@@ -4,22 +4,20 @@
 
 namespace hyped::sensors {
 
-std::optional<Temperature> Temperature::create(core::ILogger &logger,
-                                               const std::shared_ptr<io::II2c> &i2c,
-                                               const std::uint8_t device_address)
+std::optional<std::shared_ptr<Temperature>> Temperature::create(
+  core::ILogger &logger,
+  const std::shared_ptr<io::II2c> &i2c,
+  const temperatureAddress device_address)
 {
-  if (device_address != kDefaultTemperatureAddress
-      && device_address != kAlternativeTemperatureAddress) {
-    logger.log(core::LogLevel::kFatal, "Invalid device address for temperature sensor");
-    return std::nullopt;
-  }
-  const auto write_result = i2c->writeByteToRegister(device_address, kCtrl, kConfigurationSetting);
+  const auto device_address_int = static_cast<std::uint8_t>(device_address);
+  const auto write_result
+    = i2c->writeByteToRegister(device_address_int, kCtrl, kConfigurationSetting);
   if (write_result == core::Result::kFailure) {
     logger.log(core::LogLevel::kFatal, "Failed to configure temperature sensor");
     return std::nullopt;
   }
   logger.log(core::LogLevel::kDebug, "Successfully configured temperature sensor");
-  return Temperature(logger, i2c, device_address);
+  return std::make_shared<Temperature>(logger, i2c, device_address_int);
 }
 
 Temperature::Temperature(core::ILogger &logger,
@@ -66,7 +64,7 @@ std::optional<std::int16_t> Temperature::read()
     logger_.log(core::LogLevel::kFatal, "Failed to read low byte for temperature");
     return std::nullopt;
   }
-  const std::int16_t temperature = ((*temperature_high_byte << 8) | *temperature_low_byte);
+  const std::int16_t temperature = (*temperature_high_byte << 8) | *temperature_low_byte;
   logger_.log(core::LogLevel::kDebug, "Successfully read from temperature sensor");
   // Scaling temperature as per the datasheet
   return static_cast<std::int16_t>(temperature * kTemperatureScaleFactor);
