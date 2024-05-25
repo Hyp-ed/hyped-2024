ba6e9702fbcc27147d54b73e20cfe7dc332c549e
Michael Belyaev <michael7belyaev@gmail.com>
2024-05-25 19:37:41 +0100

    added topic

--- lib/core/mqtt_topics.hpp
+++ lib/core/mqtt_topics.hpp
@@ -5,19 +5,21 @@
 
 namespace hyped::core {
 
-enum class MqttTopic { kTest, kState, kAccelerometer, kOpticalFlow, kKeyence };
+enum class MqttTopic { kTest, kState, kAccelerometer, kOpticalFlow, kKeyence, kStarted };
 
 const std::unordered_map<MqttTopic, std::string> mqtt_topic_to_string
   = {{MqttTopic::kTest, "test"},
      {MqttTopic::kState, "state"},
      {MqttTopic::kAccelerometer, "accelerometer"},
      {MqttTopic::kOpticalFlow, "optical_flow"},
-     {MqttTopic::kKeyence, "keyence"}};
+     {MqttTopic::kKeyence, "keyence"},
+     {MqttTopic::kStarted, "started"}};
 const std::unordered_map<std::string, MqttTopic> mqtt_string_to_topic
   = {{"test", MqttTopic::kTest},
      {"state", MqttTopic::kState},
      {"accelerometer", MqttTopic::kAccelerometer},
      {"optical_flow", MqttTopic::kOpticalFlow},
-     {"keyence", MqttTopic::kKeyence}};
+     {"keyence", MqttTopic::kKeyence},
+     {"started", MqttTopic::kStarted};
 
 }  // namespace hyped::core
